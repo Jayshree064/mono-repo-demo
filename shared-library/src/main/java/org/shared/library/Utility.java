f11e188b17ea8ae19621ad5c624bbfce917c96c0
au137449 <jinal.joshi.wg@fujifilm.com>
2024-09-27 11:28:43 +0530

    add method in shared library project

--- shared-library/src/main/java/org/shared/library/Utility.java
+++ shared-library/src/main/java/org/shared/library/Utility.java
@@ -4,4 +4,8 @@ public class Utility {
  public static String greet(String name) {
      return "Hello, " + name + "!";
  }
+ 
+ public void test() {
+	 System.out.println("This is test method");
+ }
 }
